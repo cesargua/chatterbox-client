9ea8dff5246c852a76dcdd7857603131a9533af1
cesargua <cguajard@ucsd.edu>
2023-04-08 09:04:42 -0700

    changes

--- client/scripts/friends.js
+++ client/scripts/friends.js
@@ -5,9 +5,16 @@
 var Friends = {
   // TODO: Define how you want to store your list of friends.
 
-  _data: null,
+  _data: null, //list of friends?
 
   // TODO: Define methods which allow you to add, toggle,
   // and check the friendship status of other users.
+
+  //add method here
+
+  //toggle method here
+
+  //checkMutualFriends
+
 
 };
--- client/scripts/messages.js
+++ client/scripts/messages.js
@@ -5,9 +5,40 @@
 var Messages = {
 
   // TODO: Define how you want to store your messages.
-  _data: null,
+  _data: {}, //maybe make this into an object
+  //EXAMPLE:
+    //var _data = {
+      //username: 'shawndrost',
+      //text: 'trololo',
+      //roomname: '4chan',
+    //};
 
   // TODO: Define methods which allow you to retrieve from,
   // add to, and generally interact with the messages.
+  add: function(data) {
+    data.forEach(elem => {
+      //_data.campus = elem.campus;
+      this._data[elem.message_id] = {}
+      this._data[elem.message_id].message_id = elem.message_id;
+      this._data[elem.message_id].username = elem.username;
+      //_data.github_handle = elem.github_handle;
+      this._data[elem.message_id].text = elem.text;
+      this._data[elem.message_id].created_at = elem.created_at;
+      this._data[elem.message_id].updated_at = elem.updated_at;
+      this._data[elem.message_id].roomname = elem.roomname;
+    });
+    console.log(this._data)
+
+  },
+
+  retrieve: function(message_id, username){
+    //retrieve with get
+    // messageView would take the specific message using this function
+    // to display all messages;
+    //messagesView renderAll Message
+
+
+  }
+
 
 };
